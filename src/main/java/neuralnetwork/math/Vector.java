4d2ce24747b17f4fb8ca722064a9a04042ac7117
luthesebas <luthesebas@gmail.com>
2017-12-29 20:49:17 +0100

    Implemented Vector

--- src/main/java/neuralnetwork/math/Vector.java
+++ src/main/java/neuralnetwork/math/Vector.java
@@ -1,5 +1,6 @@
 package neuralnetwork.math;
 
+import java.security.InvalidParameterException;
 import java.util.Objects;
 
 /**
@@ -25,7 +26,92 @@ public class Vector {
     // Methods
     //--------------------------------------
 
+    public Vector negate() {
+        return this.mult(-1);
+    }
 
+    public Vector add(float scalar) {
+        float[] result = this.toArray();
+        for (int i = 0; i < result.length; i++) {
+            result[i] += scalar;
+        }
+
+        return new Vector(result);
+    }
+
+    public Vector add(Vector other) {
+        checkVectorSize(other);
+
+        float[] result = this.toArray();
+        for (int i = 0; i < result.length; i++) {
+            result[i] += other.elements[i];
+        }
+
+        return new Vector(result);
+    }
+
+    public Vector substract(float scalar) {
+        return this.add(-scalar);
+    }
+
+    public Vector substract(Vector other) {
+        return this.add(other.negate());
+    }
+
+    public Vector mult(float scalar) {
+        float[] result = this.toArray();
+        for (int i = 0; i < result.length; i++) {
+            result[i] *= scalar;
+        }
+
+        return new Vector(result);
+    }
+
+    public Vector mult(Vector other) {
+        checkVectorSize(other);
+
+        float[] result = this.toArray();
+        for (int i = 0; i < result.length; i++) {
+            result[i] *= other.elements[i];
+        }
+
+        return new Vector(result);
+    }
+
+    public Vector divide(float scalar) {
+        return this.mult(1f / scalar);
+    }
+
+    public Vector divide(Vector other) {
+        return this.mult(other.inverse());
+    }
+
+    public Vector inverse() {
+        float[] result = toArray();
+        for (int i = 0; i < result.length; i++) {
+            result[i] = 1f / result[i];
+        }
+
+        return new Vector(result);
+    }
+
+    public Vector scale(float scalar) {
+        return this.mult(scalar);
+    }
+
+    private void checkVectorSize(Vector other) {
+        if (this.numberOfElements() != other.numberOfElements()) {
+            throw new InvalidParameterException("Vectors must have the same size");
+        }
+    }
+
+    public float[] toArray() {
+        return this.elements.clone();
+    }
+
+    public int numberOfElements() {
+        return this.elements.length;
+    }
 
     //--------------------------------------
     // Getters and Setters

515eb30a77a2ff0d67a00c9c94c5c4e16bc07eae
luthesebas <luthesebas@gmail.com>
2018-01-02 20:08:57 +0100

    Update NN test

--- src/test/java/neuralnetwork/NeuralNetworkTest.java
+++ src/test/java/neuralnetwork/NeuralNetworkTest.java
@@ -3,15 +3,15 @@ package neuralnetwork;
 import neuralnetwork.math.Vector;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class NeuralNetworkTest {
 
     @Test
     public void feedForward() {
-        Vector expected = new Vector(17,39);
-        Vector input = new Vector(5,6);
-        NeuralNetwork brain = new NeuralNetwork(1,2,3,100);
+        Vector expected = new Vector(7.3597374f,7.8314123f);
+        Vector input = new Vector(5,6,7);
+        NeuralNetwork brain = new NeuralNetwork(3,2, 1, 100);
         brain.feedForward(input);
         Vector result = brain.getResult();
         assertEquals(expected, result);

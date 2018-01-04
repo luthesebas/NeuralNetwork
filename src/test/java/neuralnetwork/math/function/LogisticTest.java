39a6266da246356ff20487a795deadd92489ede3
luthesebas <luthesebas@gmail.com>
2018-01-04 11:48:47 +0100

    Added logistic test

---
+++ src/test/java/neuralnetwork/math/function/LogisticTest.java
@@ -0,0 +1,26 @@
+package neuralnetwork.math.function;
+
+import neuralnetwork.math.Vector;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogisticTest {
+
+    Logistic function = new Logistic();
+
+    @Test
+    void calculate() {
+        float expected = 0.7613327f;
+        float result = function.calculate(1.16f);
+        assertEquals(expected, result, 0.00001);
+    }
+
+    @Test
+    void calculate1() {
+        Vector expected = new Vector(0.7613327f, 0.45016602f);
+        Vector result = function.calculate(new Vector(1.16f, -0.2f));
+        assertEquals(expected, result);
+    }
+
+}

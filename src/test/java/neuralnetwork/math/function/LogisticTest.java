b9002740f3eb7a2886bf2762aa126d8e52c9e8f9
luthesebas <luthesebas@gmail.com>
2018-01-07 15:44:53 +0100

    Update Logistic test

--- src/test/java/neuralnetwork/math/function/LogisticTest.java
+++ src/test/java/neuralnetwork/math/function/LogisticTest.java
@@ -23,4 +23,32 @@ class LogisticTest {
         assertEquals(expected, result);
     }
 
+    @Test
+    void derivative() {
+        float expected = 0.24751657f;
+        float result = function.derivative(0.2f);
+        assertEquals(expected, result, 0.00001);
+    }
+
+    @Test
+    void derivative1() {
+        float expected = 0.24751657f;
+        float result = function.derivative(0.2f, 0.549834f);
+        assertEquals(expected, result, 0.00001);
+    }
+
+    @Test
+    void derivative2() {
+        Vector expected = new Vector(0.24751657f, 0.22612129f);
+        Vector result = function.derivative(new Vector(0.2f, 0.639f));
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void derivative3() {
+        Vector expected = new Vector(0.24751657f, 0.22612129f);
+        Vector result = function.derivative(new Vector(0.2f, 0.639f), new Vector(0.549834f, 0.65452737f));
+        assertEquals(expected, result);
+    }
+
 }

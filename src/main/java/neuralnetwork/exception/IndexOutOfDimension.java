ac721f5e1f6cab31dc21ee4cb4955505521bf683
luthesebas <luthesebas@gmail.com>
2018-01-18 18:12:47 +0100

    Added new Exception IndexOutOfDimension

---
+++ src/main/java/neuralnetwork/exception/IndexOutOfDimension.java
@@ -0,0 +1,20 @@
+package neuralnetwork.exception;
+
+/**
+ *
+ */
+public class IndexOutOfDimension extends RuntimeException{
+
+    //--------------------------------------
+    // Custom exception
+    //--------------------------------------
+
+    public IndexOutOfDimension() {
+        super();
+    }
+
+    public IndexOutOfDimension(String message) {
+        super(message);
+    }
+
+}
